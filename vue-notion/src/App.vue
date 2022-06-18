2886e7ece9adfe49273aaa3c51c224e9ded13e9e
felmy2020 <fyen.work@gmail.com>
2022-06-19 01:20:06 +0900

    vue-router実装

--- vue-notion/src/App.vue
+++ vue-notion/src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavigationBar />
+    <!-- URLに対応したコンポーネントを表示 -->
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavigationBar from '@/components/NavigationBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavigationBar,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
 }
 </style>
