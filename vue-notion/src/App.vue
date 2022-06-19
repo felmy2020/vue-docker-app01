cf382f52fd58d02e511cca4e1f59a9da33cda786
felmy2020 <fyen.work@gmail.com>
2022-06-19 17:01:03 +0900

    アプリ完成

--- vue-notion/src/App.vue
+++ vue-notion/src/App.vue
@@ -21,4 +21,21 @@ export default {
 #app {
   height: 100vh;
 }
+
+button.transparent {
+  margin: 5px;
+  background: transparent;
+  border: none;
+}
+
+  input.transparent {
+    width: 100%;
+    border: none;
+  }
+  input.transparent:focus {
+    outline: none;
+    -webkit-box-shadow: none;
+    box-shadow: none;
+  }
+
 </style>
